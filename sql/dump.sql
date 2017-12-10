3dc31ce8755f884768560fd4de0625a2b0e20c0f
farzaneh <farzaneh.sabzi.f17@gmail.com>
2017-12-10 23:23:01 +0100

    initial data added, bug fixed.

--- sql/dump.sql
+++ sql/dump.sql
@@ -1,8 +1,14 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- Database Dump 
+-- Farzaneh Sabzi
+-- Imam Bux 
+
+
+
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: 127.0.0.1    Database: mydb
+-- Host: localhost    Database: fa17g20
 -- ------------------------------------------------------
--- Server version	5.7.20-log
+-- Server version       5.7.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,6 +21,32 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `contact` varchar(45) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `image` longblob,
+  `user_type` tinyint DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 --
 -- Table structure for table `listing`
 --
@@ -24,10 +56,10 @@ DROP TABLE IF EXISTS `listing`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `listing` (
   `listing_id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
+  `title` varchar(70) NOT NULL,
   `description` varchar(2500) DEFAULT NULL,
   `price` int(11) NOT NULL,
-  `is_biddable` tinyint(4) NOT NULL DEFAULT '0',
+  `is_biddable` tinyint NOT NULL DEFAULT '0',
   `area` int(11) DEFAULT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '1',
   `address` varchar(255) DEFAULT NULL,
@@ -37,15 +69,16 @@ CREATE TABLE `listing` (
   `customer_id` int(11) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
-  `baths` int(11) DEFAULT '0',
-  `beds` int(11) DEFAULT '0',
-  `total_images` int(11) NOT NULL DEFAULT '0',
+  `baths` tinyint DEFAULT '0',
+  `beds` tinyint DEFAULT '0',
+  `total_images` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`listing_id`),
   KEY `agent_FK_idx` (`agent_id`),
   KEY `customer_FK_idx` (`customer_id`),
   CONSTRAINT `agent_FK` FOREIGN KEY (`agent_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `listing_images`
@@ -62,6 +95,8 @@ CREATE TABLE `listing_images` (
   CONSTRAINT `listing_FK` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`listing_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
 
 --
 -- Table structure for table `message`
@@ -87,26 +122,19 @@ CREATE TABLE `message` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+
 --
--- Table structure for table `user`
+-- Dumping data for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `contact` varchar(45) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `image` longblob,
-  `user_type` tinyint(4) DEFAULT '1',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'SF State Homes','SFStateHomes@info.de','55dc87','+49 30237890-9','Charlottenstraße 155, 10117 Berlin','?',1),
+						  (2,'SJ State Realtors','SJStateRealtors@info.de','55dc87','+49 605535434','Tränke 1, 36039 Fulda','?',1),
+                          (3,'CSU Real Estate','CSURealEstate@info.de','55dc87','+49 6589134445-7','Zentgrafenstraße 89, 34130 Kassel','?',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -117,4 +145,7 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-10 16:08:39
+-- Dump completed on 2017-12-10 22:04:47
+
+
+
