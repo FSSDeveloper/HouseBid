da6ebc5e79bd1cff86bb4bda816e97997f4e9f02
vjbhaskar <vj.bhaskar012@gmail.com>
2017-12-20 19:10:12 +0100

    bug 94 fix,added demo text

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-
+//Author- Vijay Bhaskar Puttamshetty 
 $(window).load(function () { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
